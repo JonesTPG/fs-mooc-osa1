12e4c5213a1a06862340cb8ba522965c2f84949b
JonesTPG <joonas.ryynanen1@gmail.com>
2018-07-08 17:23:15 +0300

    1.2 tehty

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Otsikko = (props) => {
+  return (
+    <div>
+      <h1> {props.kurssi}</h1>
+    </div>
+  )
+
+}
+
+
+const Sisalto = (props) => {
+  return (
+    <div>
+      <Osa nimi={props.osa1} määrä={props.tehtava1} />
+      <Osa nimi={props.osa2} määrä={props.tehtava2} />
+      <Osa nimi={props.osa3} määrä={props.tehtava3} />
+    </div>
+  )
+
+}
+
+const Yhteensa = (props) => {
+  return (
+    <div> 
+      <p>yhteensä {props.yhteensa} tehtävää </p>
+    </div>
+  )
+
+}
+
+const Osa = (props) => {
+  return (
+    <div> 
+      <p>{props.nimi} {props.määrä} </p>
+    </div>
+  )
+
+}
+
+
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
   const osa1 = 'Reactin perusteet'
@@ -12,11 +52,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{kurssi}</h1>
-      <p>{osa1} {tehtavia1}</p>
-      <p>{osa2} {tehtavia2}</p>
-      <p>{osa3} {tehtavia3}</p>
-      <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>
+      <Otsikko kurssi={kurssi} />
+      <Sisalto osa1 = {osa1} tehtava1 = {tehtavia1} 
+      osa2 = {osa2} tehtava2 ={tehtavia2}
+      osa3 = {osa3} tehtava3 = {tehtavia3} />
+      <Yhteensa yhteensa={tehtavia1+tehtavia2+tehtavia3} />
     </div>
   )
 }
