81e93abbb0d571f4470720cd980e3eb701964dd1
JonesTPG <joonas.ryynanen1@gmail.com>
2018-07-29 13:23:26 +0300

    1.7 tehty

--- src/index.js
+++ src/index.js
@@ -7,26 +7,99 @@ class App extends React.Component {
         this.state = {
             huono: 0,
             neutraali: 0,
-            hyva: 0
+            hyva: 0,
+            lkm: 0,
+            keskiarvo: 0,
+            prosentti: 0
         }
     }
+    //Koska setState-ei päivitä tilaa välittömästi, en keksinyt
+    //parempaa tapaa muuttaa keskiarvo reaaliaikaisesti kuin tämä.
+    //Tässä siis ennakoidaan muuttuva arvo riippuen siitä, mitä nappia
+    //painettiin. Sama logiikka funktiossa laskeProsentti()
+    laskeKeskiarvo(tyyppi) {
+        if (tyyppi === "hyvä") {
+            let keskiarvo;
+            keskiarvo = this.state.hyva-this.state.huono+1
+            keskiarvo = keskiarvo/(this.state.lkm+1)
+            return keskiarvo;
+        }
 
-    kasvataYhdella(tyyppi) {
+        else if (tyyppi === "neutraali") {
+            let keskiarvo;
+            keskiarvo = this.state.hyva-this.state.huono
+            keskiarvo = keskiarvo/(this.state.lkm+1)
+            return keskiarvo;
+        }
+
+        else if (tyyppi === "huono") {
+            let keskiarvo;
+            keskiarvo = this.state.hyva-this.state.huono-1
+            keskiarvo = keskiarvo/(this.state.lkm+1)
+            return keskiarvo;
+        }
+    
+    }
+
+    laskeProsentti(tyyppi) {
+        if (tyyppi === "hyvä") {
+            let prosentti;
+            prosentti = (this.state.hyva+1)/(this.state.lkm+1)
+            return prosentti*100;
+        }
+
+        else if (tyyppi === "neutraali") {
+            let prosentti;
+            prosentti = (this.state.hyva)/(this.state.lkm+1)
+            return prosentti*100;
+        }
+
+        else if (tyyppi === "huono") {
+            let prosentti;
+            prosentti = (this.state.hyva)/(this.state.lkm+1)
+            return prosentti*100;
+        }
+        
+    }
+
+    paivitaTila(tyyppi) {
+        let keskiarvo;
+        let prosentti;
+        keskiarvo = this.laskeKeskiarvo(tyyppi)
+        prosentti = this.laskeProsentti(tyyppi)
         if (tyyppi === "hyvä") {
             
-            return () => this.setState({hyva: this.state.hyva + 1})
+            return () => { 
+                this.setState({hyva: this.state.hyva + 1,
+                                lkm: this.state.lkm +1,
+                                keskiarvo: keskiarvo,
+                                prosentti: prosentti})
+ 
+            }
         }
 
         if (tyyppi === "neutraali") {
             
-            return () => this.setState({neutraali: this.state.neutraali + 1})
+            return () => { 
+                
+                this.setState({neutraali: this.state.neutraali + 1,
+                                lkm: this.state.lkm +1,
+                                keskiarvo: keskiarvo,
+                                prosentti: prosentti})
+            }
         }
 
         if (tyyppi === "huono") {
             
-            return () => this.setState({huono: this.state.huono + 1})
+            return () =>  { 
+                this.setState({huono: this.state.huono + 1,
+                                        lkm: this.state.lkm +1,
+                                        keskiarvo: keskiarvo,
+                                        prosentti: prosentti})
+            }
+        
         }
-    }
+}
 
     render() {
         return (
@@ -34,15 +107,15 @@ class App extends React.Component {
                 <h3>Anna palautetta</h3>
 
                 <Button
-                    handleClick={this.kasvataYhdella("hyvä")}
+                    handleClick={this.paivitaTila("hyvä")}
                     text="hyvä"
                 />
                 <Button
-                    handleClick={this.kasvataYhdella("neutraali")}
+                    handleClick={this.paivitaTila("neutraali")}
                     text="neutraali"
                 />
                 <Button
-                    handleClick={this.kasvataYhdella("huono")}
+                    handleClick={this.paivitaTila("huono")}
                     text="huono"
                 />
 
@@ -52,6 +125,9 @@ class App extends React.Component {
                     hyva={this.state.hyva}
                     neutraali={this.state.neutraali}
                     huono={this.state.huono}
+                    keskiarvo={this.state.keskiarvo.toFixed(1)}
+                    lkm={this.state.lkm}
+                    prosentti={this.state.prosentti.toFixed(1)}
                 />
 
             </div>
@@ -64,7 +140,10 @@ const Display = (props) => {
         <div>
             <p>hyvä {props.hyva}<br/>
             neutraali {props.neutraali}<br/> 
-            huono {props.huono}</p> 
+            huono {props.huono}<br/>
+            keskiarvo {props.keskiarvo}<br/>
+            lkm {props.lkm}<br/>
+            positiivisia {props.prosentti} %<br/></p>
         </div>
     )
 }
